2f5ab9d47cb1e16d42df5a0b82cbf074a8dbde31
Zeus Ricote <zeusricote@gmail.com>
2022-10-13 17:01:25 +0200

    lets gooooo

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Yggdrassil
+Succesor of github.com/ankris812/Korai
+
+I'll be updating this when i make changes, until then, have a good day people
+FIrst of all. Thanks to Flipper Devices, for this amazing tool, and its firmware.
---
+++ applications/services/desktop/views/desktop_events.h
@@ -0,0 +1,48 @@
+#pragma once
+
+typedef enum {
+    DesktopMainEventOpenLockMenu,
+    DesktopMainEventOpenArchive,
+    DesktopMainEventOpenFavoritePrimary,
+    DesktopMainEventOpenFavoriteSecondary,
+    DesktopMainEventOpenMenu,
+    DesktopMainEventOpenDebug,
+    DesktopMainEventOpenPassport,
+    DesktopMainEventOpenPowerOff,
+    DesktopMainEventLock,
+
+    DesktopMainEventOpenGameMenu,
+
+    DesktopLockedEventUnlocked,
+    DesktopLockedEventUpdate,
+    DesktopLockedEventShowPinInput,
+
+    DesktopPinInputEventResetWrongPinLabel,
+    DesktopPinInputEventUnlocked,
+    DesktopPinInputEventUnlockFailed,
+    DesktopPinInputEventBack,
+
+    DesktopPinTimeoutExit,
+
+    DesktopDebugEventDeed,
+    DesktopDebugEventWrongDeed,
+    DesktopDebugEventSaveState,
+    DesktopDebugEventExit,
+
+    DesktopLockMenuEventLock,
+    DesktopLockMenuEventPinLock,
+    DesktopLockMenuEventDummyModeOn,
+    DesktopLockMenuEventDummyModeOff,
+
+    DesktopAnimationEventCheckAnimation,
+    DesktopAnimationEventNewIdleAnimation,
+    DesktopAnimationEventInteractAnimation,
+
+    DesktopSlideshowCompleted,
+    DesktopSlideshowPoweroff,
+
+    // Global events
+    DesktopGlobalBeforeAppStarted,
+    DesktopGlobalAfterAppFinished,
+    DesktopGlobalAutoLock,
+} DesktopEvent;
